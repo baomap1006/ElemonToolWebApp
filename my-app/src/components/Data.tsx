eb9ddbd3aaaf89985be5b45a28cb20b5b6d3346c
bao <baomap1006@gmail.com>
2022-06-30 03:42:58 -0400

    change time format to 'time' ago

--- my-app/src/components/Data.tsx
+++ my-app/src/components/Data.tsx
@@ -36,7 +36,34 @@ function Data({}: Props) {
       cancel = true;
     };
   }, []);
-
+  function timeSince(date:string) {
+    let startTime=new Date(date.toString()).getTime()
+    let end = new Date().getTime();
+    var seconds = Math.floor(( end- startTime) / 1000);
+  
+    var interval = seconds / 31536000;
+  
+    if (interval > 1) {
+      return Math.floor(interval) + " years";
+    }
+    interval = seconds / 2592000;
+    if (interval > 1) {
+      return Math.floor(interval) + " months";
+    }
+    interval = seconds / 86400;
+    if (interval > 1) {
+      return Math.floor(interval) + " days";
+    }
+    interval = seconds / 3600;
+    if (interval > 1) {
+      return Math.floor(interval) + " hours";
+    }
+    interval = seconds / 60;
+    if (interval > 1) {
+      return Math.floor(interval) + " minutes";
+    }
+    return Math.floor(seconds) + " seconds";
+  }
   return (
     <div className="grid ">
       {/* <code>{JSON.stringify(elemons)}</code> */}
@@ -57,10 +84,12 @@ function Data({}: Props) {
             {elemons?.length > 0 &&
               elemons.map((elemon) => {
                 let txLink: string = `https://bscscan.com/tx/${elemon.transaction_hash}`;
+                
+                let timeString = timeSince(elemon.block_timestamp.toString()) + " ago"
                 return (
                   <tr key={elemon.transaction_hash} className="table-row gap-2 w-full hover:bg-indigo-400 odd:bg-white even: bg-slate-100" >
                     <td className="table-cell">{elemon.tokenId}</td>
-                    <td className="table-cell">{elemon.block_timestamp}</td>
+                    <td className="table-cell">{timeString}</td>
                     <td className="table-cell">{elemon.from}</td>
                     <td className="table-cell ">
                       <a className=" overflow-hidden text-ellipsis whitespace-nowrap w-60 inline-block  " href={txLink} rel="noreferrer" target="_blank">
